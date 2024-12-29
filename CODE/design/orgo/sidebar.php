dad7eba6e9d32fb70720d57e0e6edf5be422e80b
Hasan Abouminyar <among.00.12@gmail.com>
2024-12-29 04:03:34 +0200

    إدارة الطلبات

--- CODE/design/orgo/sidebar.php
+++ CODE/design/orgo/sidebar.php
@@ -35,7 +35,7 @@
     <li class="nav-item">
       <a class="nav-link " href="request.php">
         <i class="bi bi-grid"></i>
-        <span>وظيفة التقديم وقبول الطلبات </span>
+        <span>إدارة الطلبات</span>
       </a>
     </li>
     <li class="nav-item">
