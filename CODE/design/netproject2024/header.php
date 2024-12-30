773010349a1ee0f8f3cebfa28f5a11450b900848
Hasan Abouminyar <among.00.12@gmail.com>
2024-12-30 17:30:24 +0200

    تعديل الهدر  في صفحة المتطوع

--- CODE/design/netproject2024/header.php
+++ CODE/design/netproject2024/header.php
@@ -1,139 +1,87 @@
-<!-- ======= Header ======= -->
-<header id="header" class="header fixed-top d-flex align-items-center">
+<header id="header" class="header fixed-top d-flex align-items-center" dir="rtl">
 
-  <div class="d-flex align-items-center justify-content-between">
-    <a href="index.html" class="logo d-flex align-items-center">
-      <img src="assets/img/logo.png" alt="">
-      <span class="d-none d-lg-block">NiceAdmin</span>
-    </a>
-    <i class="bi bi-list toggle-sidebar-btn"></i>
-  </div><!-- End Logo -->
+    <div class="d-flex align-items-center justify-content-between">
+      <a href="index.html" class="logo d-flex align-items-center">
+        <img src="assets/img/logo.png" alt="شعار">
+        <span class="d-none d-lg-block">NiceAdmin</span>
+      </a>
+      <i class="bi bi-list toggle-sidebar-btn"></i>
+    </div><!-- Logo -->
 
-  <div class="search-bar">
-    <form class="search-form d-flex align-items-center" method="POST" action="#">
-      <input type="text" name="query" placeholder="Search" title="Enter search keyword">
-      <button type="submit" title="Search"><i class="bi bi-search"></i></button>
-    </form>
-  </div><!-- End Search Bar -->
+    <div class="search-bar">
+      <form class="search-form d-flex align-items-center" method="POST" action="#">
+        <input type="text" name="query" placeholder="بحث" title="أدخل كلمة البحث">
+        <button type="submit" title="بحث"><i class="bi bi-search"></i></button>
+      </form>
+    </div><!-- شريط البحث -->
 
-  <nav class="header-nav ms-auto">
-    <ul class="d-flex align-items-center">
+    <nav class="header-nav ms-auto">
+      <ul class="d-flex align-items-center">
 
-      <li class="nav-item d-block d-lg-none">
-        <a class="nav-link nav-icon search-bar-toggle " href="#">
-          <i class="bi bi-search"></i>
-        </a>
-      </li><!-- End Search Icon-->
+        <li class="nav-item">
+  <a class="nav-link nav-icon" href="notification.php">
+    <i class="bi bi-bell"></i>
+    <span class="badge bg-primary badge-number">4</span>
+  </a>
+</li><!-- الإشعارات -->
 
-      <li class="nav-item dropdown" style="margin-left: 70px;">
+        <li class="nav-item dropdown pe-3">
 
-        <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
-          <i class="bi bi-bell"></i>
-          <span class="badge bg-primary badge-number">4</span>
-        </a><!-- End Notification Icon -->
+          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="assets/img/profile-img.jpg" alt="الملف الشخصي" class="rounded-circle">
+            <span class="d-none d-md-block dropdown-toggle ps-2">كيفن أندرسون</span>
+          </a><!-- أيقونة الملف الشخصي -->
 
-        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
+          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile" style="left: auto; right: 0;">
+            <li class="dropdown-header">
+              <h6>كيفن أندرسون</h6>
+              <span>مصمم ويب</span>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
 
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="volunteer_profile.php">
+                <i class="bi bi-person"></i>
+                <span>ملفي الشخصي</span>
+              </a>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
 
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                <i class="bi bi-gear"></i>
+                <span>إعدادات الحساب</span>
+              </a>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
 
-          <li class="notification-item">
-            <i class="bi bi-exclamation-circle text-warning"></i>
-            <div>
-              <h4>تحديث بيانات الحساب</h4>
-              <p></p>
-              <p> قبل 30 د</p>
-            </div>
-          </li>
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
+                <i class="bi bi-question-circle"></i>
+                <span>هل تحتاج إلى مساعدة؟</span>
+              </a>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
 
-          <li>
-            <hr class="dropdown-divider">
-          </li>
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="login.php">
+                <i class="bi bi-box-arrow-right"></i>
+                <span>تسجيل الخروج</span>
+              </a>
+            </li>
 
-          <li class="notification-item">
-            <i class="bi bi-x-circle text-danger"></i>
-            <div>
-              <h4>رفض الطلب</h4>
-              <p>لقد تم رفضك في الفاعاليه</p>
-              <p>قبل ساعة</p>
-            </div>
-          </li>
+          </ul><!-- قائمة الملف الشخصي -->
+        </li><!-- الملف الشخصي -->
 
-          <li>
-            <hr class="dropdown-divider">
-          </li>
+      </ul>
+    </nav><!-- التنقل بالأيقونات -->
 
-          <li class="notification-item">
-            <i class="bi bi-check-circle text-success"></i>
-            <div>
-              <h4>قبول الطلب </h4>
-              <p>لقد تم قبولك في الفاعليه</p>
-              <p>قبل ساعتان</p>
-            </div>
-          </li>
-
-          <li>
-            <hr class="dropdown-divider">
-          </li>
-
-          <li class="notification-item">
-            <i class="bi bi-info-circle text-primary"></i>
-            <div>
-              <h4>اشعارات اخرى</h4>
-              <p>تتضمن اشعارا </p>
-              <p>قبل 4ساعات</p>
-            </div>
-          </li>
-
-          <li>
-            <hr class="dropdown-divider">
-          </li>
-          <li class="">
-            <a href="notification.php">عرض جميع الاشعارات</a>
-          </li>
-
-        </ul><!-- End Notification Dropdown Items -->
-
-      </li><!-- End Notification Nav -->
-
-
-
-      <li class="nav-item dropdown pe-3">
-
-        <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-          <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-          <span class="d-none d-md-block dropdown-toggle ps-2">ااسم المتطوع</span>
-        </a><!-- End Profile Iamge Icon -->
-
-        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-          <li class="dropdown-header">
-            <h6>اسم المتطوع</h6> 
-
-          </li>
-          <li>
-            <hr class="dropdown-divider">
-          </li>
-
-          <li>
-            <a class="dropdown-item d-flex align-items-center" href="volunteer_profile.php">
-              <i class="bi bi-person"></i>
-              <span>الملف الشخصي</span>
-            </a>
-          </li>
-          <li>
-            <hr class="dropdown-divider">
-          </li>
-
-          <li>
-            <a class="dropdown-item d-flex align-items-center" href="login.php">
-              <i class="bi bi-box-arrow-right"></i>
-              <span>تسجيل الخروج</span>
-            </a>
-          </li>
-
-        </ul><!-- End Profile Dropdown Items -->
-      </li><!-- End Profile Nav -->
-
-    </ul>
-  </nav><!-- End Icons Navigation -->
-
-</header><!-- End Header -->
+  </header>
