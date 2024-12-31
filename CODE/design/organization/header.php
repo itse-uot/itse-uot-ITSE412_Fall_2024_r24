2150dbba7de40b32ce3f7fb1f669a4fd6efbb260
ahmed <sharefahmed005@gmail.com>
2024-12-31 16:44:38 +0200

    قوائم البحث

--- CODE/design/organization/header.php
+++ CODE/design/organization/header.php
@@ -1,77 +1,90 @@
 <header id="header" class="header fixed-top d-flex align-items-center" dir="rtl">
 
-    <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
-        <img src="assets/img/logo.png" alt="شعار">
-        <span class="d-none d-lg-block">NiceAdmin</span>
-      </a>
-      <i class="bi bi-list toggle-sidebar-btn"></i>
-    </div><!-- Logo -->
+  <div class="d-flex align-items-center justify-content-between">
+    <a href="index.html" class="logo d-flex align-items-center">
+      <img src="assets/img/logo.png" alt="شعار">
+      <span class="d-none d-lg-block">NiceAdmin</span>
+    </a>
+    <i class="bi bi-list toggle-sidebar-btn"></i>
+  </div><!-- Logo -->
 
-    <div class="search-bar">
-      <form class="search-form d-flex align-items-center" method="POST" action="#">
-        <input type="text" name="query" placeholder="بحث" title="أدخل كلمة البحث">
-        <button type="submit" title="بحث"><i class="bi bi-search"></i></button>
-      </form>
-    </div><!-- شريط البحث -->
+  <div class="search-bar">
+    <form class="search-form d-flex align-items-center" method="POST" action="#">
+      <input type="text" name="query" placeholder="بحث" title="أدخل كلمة البحث">
+      <button type="submit" title="بحث"><i class="bi bi-search"></i></button>
+    </form>
+  </div><!-- شريط البحث -->
+  <!-- Example single danger button -->
+  <!-- زر القائمة المنسدلة -->
+  <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+    الفاعاليات
+  </button>
 
-    <nav class="header-nav ms-auto">
-      <ul class="d-flex align-items-center">
+  <!-- القائمة المنسدلة -->
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">الفاعاليات</a>
+    <a class="dropdown-item" href="#">المتطوعين</a>
+    <a class="dropdown-item" href="#">المنظمات</a>
+  </div>
 
-        <li class="nav-item">
-  <a class="nav-link nav-icon" href="notification.php">
-    <i class="bi bi-bell"></i>
-    <span class="badge bg-primary badge-number">4</span>
-  </a>
-</li><!-- الإشعارات -->
 
-        <li class="nav-item dropdown pe-3">
+  <nav class="header-nav ms-auto">
+    <ul class="d-flex align-items-center">
 
-          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="assets/img/profile-img.jpg" alt="الملف الشخصي" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">كيفن أندرسون</span>
-          </a><!-- أيقونة الملف الشخصي -->
+      <li class="nav-item">
+        <a class="nav-link nav-icon" href="notification.php">
+          <i class="bi bi-bell"></i>
+          <span class="badge bg-primary badge-number">4</span>
+        </a>
+      </li><!-- الإشعارات -->
 
-          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile" style="left: auto; right: 0;">
-            <li class="dropdown-header">
-              <h6>كيفن أندرسون</h6>
-              <span>مصمم ويب</span>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
+      <li class="nav-item dropdown pe-3">
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="volunteer_profile.php">
-                <i class="bi bi-person"></i>
-                <span>ملفي الشخصي</span>
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
+        <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+          <img src="assets/img/profile-img.jpg" alt="الملف الشخصي" class="rounded-circle">
+          <span class="d-none d-md-block dropdown-toggle ps-2">كيفن أندرسون</span>
+        </a><!-- أيقونة الملف الشخصي -->
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="../volunteer/dashboard.php">
+        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile" style="left: auto; right: 0;">
+          <li class="dropdown-header">
+            <h6>كيفن أندرسون</h6>
+            <span>مصمم ويب</span>
+          </li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+
+          <li>
+            <a class="dropdown-item d-flex align-items-center" href="volunteer_profile.php">
+              <i class="bi bi-person"></i>
+              <span>ملفي الشخصي</span>
+            </a>
+          </li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+
+          <li>
+            <a class="dropdown-item d-flex align-items-center" href="../volunteer/dashboard.php">
               <i class="ri-arrow-go-forward-line"></i>
-                <span>العودة إلى متطوع</span>
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
+              <span>العودة إلى متطوع</span>
+            </a>
+          </li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
 
-            <li>
-              <a class="dropdown-item d-flex align-items-center" href="login.php">
-                <i class="bi bi-box-arrow-right"></i>
-                <span>تسجيل الخروج</span>
-              </a>
-            </li>
+          <li>
+            <a class="dropdown-item d-flex align-items-center" href="login.php">
+              <i class="bi bi-box-arrow-right"></i>
+              <span>تسجيل الخروج</span>
+            </a>
+          </li>
 
-          </ul><!-- قائمة الملف الشخصي -->
-        </li><!-- الملف الشخصي -->
+        </ul><!-- قائمة الملف الشخصي -->
+      </li><!-- الملف الشخصي -->
 
-      </ul>
-    </nav><!-- التنقل بالأيقونات -->
+    </ul>
+  </nav><!-- التنقل بالأيقونات -->
 
-  </header>
+</header>
